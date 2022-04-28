6c83e61f68b1044c4bb2cd032298f5b234f44626
DaltonHart <daltonvaren@gmail.com>
2022-04-28 08:03:16 -0700

    :heavy_plus_sign: (morgan) adds morgan for logs

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import express from "express";
+import morgan from "morgan";
 
 const app = express();
+
+app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
   res.json({ message: "Heck ya docker!! 🐳" });
